1ee2de759a3d8bf153d38843ecb18a72328162c6
Mohan-Zhang-u <mohan.zhang.mz@gmail.com>
2022-08-21 22:15:09 +0800

    ver update

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name='rlgridworld',
-    version='0.1001',
+    version='0.1002',
     author="Mohan Zhang",
     author_email="mohan.zhang.mz@gmail.com",
     description="This is a simple yet efficient, highly customizable grid-world implementation to run reinforcement learning algorithms.",
